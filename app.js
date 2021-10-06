7f9bf666cfe9e585f4c44f4173000302bb00026d
Benatovich <sambenatovich@gmail.com>
2021-10-06 17:02:29 -0400

    lab 03a without optional content

--- app.js
+++ app.js
@@ -1,10 +1,25 @@
 const updateButton = document.getElementById('update-button');
 const nameInput = document.getElementById('name-input');
 const nameDisplay = document.getElementById('name-display');
-
+console.log(updateButton, nameInput, nameDisplay);
 
 updateButton.addEventListener('click', () => {
-    // this code runs when button is clicked
+    // this code runs when update button is clicked
     const newName = nameInput.value;
+    
     nameDisplay.textContent = newName;
-});
+})
+
+const redButton = document.getElementById('red-button');
+const blueButton = document.getElementById('blue-button');
+const greenButton = document.getElementById('green-button');
+console.log(redButton, blueButton, greenButton);
+
+redButton.addEventListener('click', () => {
+    // this code runs when red button is clicked
+    
+
+})
+
+
+;
